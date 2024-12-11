af2ffb9c7ce03a109f7a248899302f9c86b7e830
UDanV <danil17092006@gmail.com>
2024-12-11 11:57:27 +0300

    added buttons to upload

--- frontend/src/api/courses.ts
+++ frontend/src/api/courses.ts
@@ -8,3 +8,14 @@ export const getCourses = async () => {
     console.log('Ошибка', error);
   }
 };
+
+export const createCourse = async (title: string, description: string) => {
+  const data = { title, description };
+
+  try {
+    const response = await axiosInstance.post('/courses/', data);
+    return response.data;
+  } catch (error) {
+    console.log('Ошибка', error);
+  }
+};
--- frontend/src/components/courses-items.tsx
+++ frontend/src/components/courses-items.tsx
@@ -1,10 +1,17 @@
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getCourses } from '../api/courses';
+import { getCourses, createCourse } from '../api/courses';
 import { Course } from '../types/course';
+import { getCurrentUser } from "../api/user";
 
 export const CoursesItems = () => {
-  const [courses, setCourses] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [courses, setCourses] = useState<Course[]>([]);
+  const [role, setRole] = useState<string>('');
+  const [, setLoadingRole] = useState(true);
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
 
   const navigate = useNavigate();
 
@@ -19,29 +26,93 @@ export const CoursesItems = () => {
     };
     fetchCourses();
   }, []);
+
+  useEffect(() => {
+    const fetchUserRole = async () => {
+      try {
+        const user = await getCurrentUser();
+        console.log('Полученная роль:', user.role);
+        setRole(user.role);
+      } catch (error) {
+        console.log('Ошибка при получении пользователя', error);
+      } finally {
+        setLoadingRole(false);
+      }
+    };
+
+    fetchUserRole();
+  }, []);
+
+  const handleAddCourse = async () => {
+    try {
+      await createCourse(title, description);
+      setIsModalOpen(false);
+    } catch (error) {
+      console.log('Ошибка при добавлении курса', error);
+    }
+  };
+
   return (
-    <section className="courses">
-      <div className="container">
-        <div className="courses__title">Курсы</div>
-        <div className="courses__items">
-          {courses.map((el: Course) => (
-            <div onClick={() => navigate(`/tasks/${el.id}`)} key={el.id} className="courses__item">
-              <img
-                src="https://blog.coursify.me/wp-content/uploads/2018/08/plan-your-online-course.jpg"
-                alt="course"
-                className="courses__item-img"
-              />
-              <div className="courses__item-content">
-                {' '}
-                <div className="courses__item-title">{el.title}</div>
-                <div className="courses__item-desc">{el.description}</div>
-                {/*<div style={{ display: 'flex', justifyContent: 'center' }}>*/}
-                {/*</div>*/}
+      <section className="courses">
+        <div className="container">
+          <div className="courses__title">Курсы</div>
+          {(role === 'Админ' || role === 'Учитель') && (
+              <button className="courses__button" onClick={() => setIsModalOpen(true)}>
+                Добавить курс
+              </button>
+          )}
+
+          {isModalOpen && (
+              <div className="vacancies-page__modal">
+                <div className="vacancies-page__modal-content">
+                  <button type="button" onClick={() => setIsModalOpen(false)}>
+                    &times;
+                  </button>
+                  <h2>Добавить курс</h2>
+                  <form onSubmit={(e) => e.preventDefault()}>
+                    <label>
+                      Название курса:
+                      <input
+                          type="text"
+                          value={title}
+                          onChange={(e) => setTitle(e.target.value)}
+                          required
+                      />
+                    </label>
+                    <label>
+                      Описание курса:
+                      <input
+                          value={description}
+                          onChange={(e) => setDescription(e.target.value)}
+                          required
+                      />
+                    </label>
+                    <div className="vacancies-page__modal-actions">
+                      <button type="submit" onClick={handleAddCourse}>
+                        Добавить
+                      </button>
+                    </div>
+                  </form>
+                </div>
               </div>
-            </div>
-          ))}
+          )}
+
+          <div className="courses__items">
+            {courses.map((el: Course) => (
+                <div onClick={() => navigate(`/tasks/${el.id}`)} key={el.id} className="courses__item">
+                  <img
+                      src="https://blog.coursify.me/wp-content/uploads/2018/08/plan-your-online-course.jpg"
+                      alt="course"
+                      className="courses__item-img"
+                  />
+                  <div className="courses__item-content">
+                    <div className="courses__item-title">{el.title}</div>
+                    <div className="courses__item-desc">{el.description}</div>
+                  </div>
+                </div>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
   );
 };
